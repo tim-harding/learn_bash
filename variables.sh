4c14b9aee2a9261cedb2ad284c2739e93274008c
Tim Harding <tim@timharding.co>
2022-12-13 19:58:27 -0800

    Variables

--- variables.sh
+++ variables.sh
@@ -1,12 +1,55 @@
-#!/bin/bash
+#! /bin/bash
 
-file=thing/$(date +%Y%m%d).tar.gz
-echo $file
+a=5
+a+=2
+echo "$a" # 52
 
-function bash {
-    local var="Local"
-    echo $var
-}
+a=5
+let a+=2
+echo "$a" # 7
 
-bash
-echo $var # Not defined, prints nothing
+declare -a array
+declare -A dict
+
+for item in "$*"
+do
+    echo "$item" # "a b c"
+done
+
+for item in "$@"
+do
+    echo "$item" # "a" "b" "c"
+done
+
+echo "$#" # 3
+echo "$$" # PID of shell
+echo "$?" # Exit code of last command, 0
+
+echo "${thing:-word}" # Substitute when unset
+thing="thing"
+echo "${thing:+word}" # Substitute when set
+echo "${#thing}" # Length
+
+thing="things and stuff and things and stuff"
+# Shortest matches deleted from beginning or end
+echo "${thing#*and }" # stuff and things and stuff
+echo "${thing%and*}" # things and stuff and things
+# Longest matches (greedy)
+echo "${thing##*and }" # stuff
+echo "${thing%%and*}" # things
+# Replacement, first or all
+echo "${thing/and/but}"
+echo "${thing//and/but}"
+array=("things", "and", "stuff")
+# Useful for common prefixes and suffixes
+echo "${array[@]/#/prefix }" # Match against start
+echo "${array[@]/%/ suffix}" # Match against end, not fully understanding this one
+
+# # refers to beginning
+# % refers to end
+# Doubling causes greediness
+
+ping google.com &
+echo "$!" # PID of last background command
+sleep 1
+kill $!
